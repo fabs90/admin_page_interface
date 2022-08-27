f992d5496c7bebe87fbd76ba758332877ef49281
Fabs90 <fabianjuliansyah89@gmail.com>
2022-08-27 13:26:17 +0700

    first commit

---
+++ dashboard.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Akademik Sistem Informasi</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+
+    <!-- Css Link  -->
+    <link rel="stylesheet" href="admin.css">
+
+     <!--load all Font Awesome styles -->
+  <link href="fontawesome/css/all.css" rel="stylesheet">
+  </head>
+  <body>
+
+<!-- Navbar -->
+<section id="navbar">
+<nav class="navbar navbar-expand-lg bg-warning fixed-top">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Selamat Datang Admin | Universitas Gunadarma</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="icon">
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto me-4">
+            <li class="nav-item mx-2">
+              <a class="nav-link active" href="#"><i class="fa-solid fa-envelope"></i></a>
+            </li>
+            <li class="nav-item mx-2">
+              <a class="nav-link active" href="#"><i class="fa-solid fa-bell"></i></a>
+            </li>
+            <li class="nav-item mx-2">
+              <a class="nav-link active" href="#"><i class="fa-solid fa-arrow-right-from-bracket"></i></a>
+            </li>
+        </ul>
+    </div>
+    </div>
+  </div>
+</nav>
+</section>
+
+    <!-- Link JS -->
+    <script type="text/javascript" src="admin.js"></script>
+    <!-- all.js loads all styles and icons -->
+  <script defer src="fontawesome/js/all.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  </body>
+</html>
