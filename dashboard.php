5bd3fc09acdc0072ebd85c44d5edf168079953fa
Fabs90 <fabianjuliansyah89@gmail.com>
2022-09-13 22:06:43 +0700

    menambah icon pada web

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,9 @@
     <title>Akademik Sistem Informasi</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 
+  <!-- Icon Web -->
+  <link rel="shortcut icon" href="fontawesome/house-solid.svg" type="image/x-icon">
+
     <!-- Css Link  -->
     <link rel="stylesheet" href="style.css">
 
